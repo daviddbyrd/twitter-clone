b51440ed069d67e849c45bfc469ee60af74e7280
daviddbyrd <davbywork@gmail.com>
2025-05-02 16:34:28 +0100

    Add Auth context provider.

--- client/twitter-clone/src/App.tsx
+++ client/twitter-clone/src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthPage from "./views/AuthPage";
 import LogInPage from "./views/LogInPage";
 import SignUpPage from "./views/SignUpPage";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="login" element={<LogInPage />} />
-        <Route path="signup" element={<SignUpPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthPage />} />
+          <Route path="login" element={<LogInPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ client/twitter-clone/src/context/AuthContext.tsx
@@ -0,0 +1,55 @@
+import { createContext, useState, useEffect, ReactNode } from "react";
+import { jwtDecode } from "jwt-decode";
+
+interface User {
+  id: string;
+  exp: number;
+  iat: number;
+}
+
+interface AuthContextType {
+  user: User | null;
+  isLoggedIn: boolean;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      try {
+        const decoded = jwtDecode<User>(token);
+        const isExpired = decoded.exp * 1000 < Date.now();
+
+        if (isExpired) {
+          localStorage.removeItem("token");
+        } else {
+          setUser(decoded);
+          setIsLoggedIn(true);
+        }
+      } catch (err) {
+        console.error("Error decoding token", err);
+        localStorage.removeItem("token");
+      }
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, isLoggedIn, setUser, setIsLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

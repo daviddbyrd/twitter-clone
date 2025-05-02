f983ac60af95c013e59c6d5aabc5ea9f178f5a9b
daviddbyrd <davbywork@gmail.com>
2025-05-02 16:39:56 +0100

    Set up basic test of auth context in feed.

--- client/twitter-clone/src/App.tsx
+++ client/twitter-clone/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthPage from "./views/AuthPage";
 import LogInPage from "./views/LogInPage";
 import SignUpPage from "./views/SignUpPage";
+import Feed from "./components/Feed";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<AuthPage />} />
           <Route path="login" element={<LogInPage />} />
           <Route path="signup" element={<SignUpPage />} />
+          <Route path="feed" element={<Feed />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
--- client/twitter-clone/src/components/Feed.tsx
+++ client/twitter-clone/src/components/Feed.tsx
@@ -1,46 +1,11 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Feed = () => {
-  const [postText, setPostText] = useState<string>("");
-  const [data, setData] = useState();
-
-  const fetchData = () => {
-    axios.get("http://localhost:3001/posts").then((res) => {
-      setData(res.data);
-      console.log(res.data);
-    });
-  };
-
-  const sendPostToDB = async () => {
-    console.log(postText);
-    try {
-      await axios.post("http://localhost:3001/posts", {
-        post: postText,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const makePost = async () => {
-    await sendPostToDB();
-    setPostText("");
-  };
+  const auth = useContext(AuthContext);
 
   return (
-    <div className="w-4/10 h-full flex flex-col">
-      <div>
-        <input
-          className="w-7/10 border-1 border-black rounded-xs"
-          value={postText}
-          onChange={(e) => setPostText(e.target.value)}
-        />
-        <button onClick={makePost}>Post</button>
-      </div>
-      <button onClick={fetchData}>Fetch</button>
-      {data && <div>{JSON.stringify(data)}</div>}
-    </div>
+    <div className="w-4/10 h-full flex flex-col">{JSON.stringify(auth)}</div>
   );
 };
 

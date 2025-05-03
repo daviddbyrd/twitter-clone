797ce168c024d4e26f23288c89614b8999bba1d8
daviddbyrd <davbywork@gmail.com>
2025-05-03 17:19:42 +0100

    Reduce height of user component.

--- client/twitter-clone/src/components/User.tsx
+++ client/twitter-clone/src/components/User.tsx
@@ -7,15 +7,15 @@ interface UserProps {
 
 const User = ({ id, username, displayName, handleFollow }: UserProps) => {
   return (
-    <div className="w-full h-30 bg-white border-3 border-gray-100 flex flex-row items-center rounded-lg m-4">
-      <div className="w-30 h-30 flex items-center justify-center">
+    <div className="w-full h-20 bg-white border-3 border-gray-100 flex flex-row items-center rounded-lg m-4">
+      <div className="w-20 h-20 flex items-center justify-center">
         <img
           src="/images/profilepic.png"
           alt="Profile picture"
-          className="w-20 h-20 rounded-full mr-auto ml-5"
+          className="w-12 h-12 rounded-full mr-auto ml-5"
         />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col ml-2">
         <div>
           <h1 className="text-lg font-bold">{username}</h1>
         </div>

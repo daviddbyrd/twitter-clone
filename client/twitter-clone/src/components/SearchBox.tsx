330e4c2b1e526cc1d68dc49d33fca03c87547114
daviddbyrd <davbywork@gmail.com>
2025-05-04 18:03:50 +0100

    Add styling and functionality to search box, so that user can search by pressing enter.

--- client/twitter-clone/src/components/SearchBox.tsx
+++ client/twitter-clone/src/components/SearchBox.tsx
@@ -1,5 +1,6 @@
 import { useState, ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 const SearchBox = () => {
   const [query, setQuery] = useState<string>("");
@@ -9,14 +10,27 @@ const SearchBox = () => {
     setQuery(e.target.value);
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   const handleSearch = () => {
     navigate(`/search/${encodeURIComponent(query)}`);
   };
 
   return (
-    <div>
-      <input type="text" value={query} onChange={handleChange} />
-      <button onClick={handleSearch}>Search</button>
+    <div className="ml-10 mt-5 h-8 w-full rounded-full border-2 border-gray-200 flex flex-row items-center justify-center">
+      <FaSearch className="w-5 h-5 text-gray-300 mr-auto ml-2" />
+      <input
+        className="w-full pl-2 focus:outline-none"
+        type="text"
+        value={query}
+        placeholder="Search"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
     </div>
   );
 };

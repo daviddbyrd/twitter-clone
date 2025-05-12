539002afb0efd371a52bf6530781228905ea3983
daviddbyrd <davbywork@gmail.com>
2025-05-12 13:54:30 +0100

    Add bio section to edit profile box.

--- client/twitter-clone/src/components/EditProfileBox.tsx
+++ client/twitter-clone/src/components/EditProfileBox.tsx
@@ -57,19 +57,35 @@ const EditProfileBox = ({
               className="rounded-full w-36 h-36 flex items-center justify-center absolute left-12 bottom-0 transform translate-y-1/2 border-white border-4"
             />
           </div>
-          <div className="w-full px-5 mt-24">
-            <div className="relative w-full">
+          <div className="w-full px-5 mt-24 h-14">
+            <div className="relative w-full h-full">
               <input
                 id="displayName"
-                className="peer w-full h-14 border border-gray-300 rounded-sm px-3 pb-2 pt-5 placeholder-transparent focus:outline-none focus:border-blue-500 resize-none"
+                className="peer w-full h-full border border-gray-300 rounded-sm px-3 pb-2 pt-5 placeholder-transparent focus:outline-none focus:border-blue-500"
                 name="displayName"
                 placeholder="Name"
               />
               <label
                 htmlFor="displayName"
-                className="pointer-events-none absolute left-3 top-3 transform -translate-y-1/2 text-gray-200 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:top-3 peer-focus:text-xs peer-focus:text-sky-500"
+                className="pointer-events-none absolute left-3 top-3 transform -translate-y-1/2 text-xs transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg peer-placeholder-shown:text-gray-500 peer-focus:top-3 peer-focus:text-xs peer-focus:text-sky-500"
               >
                 Name
+              </label>
+            </div>
+          </div>
+          <div className="w-full px-5 mt-7 h-40">
+            <div className="relative w-full h-full">
+              <textarea
+                id="displayName"
+                className="peer w-full h-full border border-gray-300 rounded-sm px-3 pb-2 pt-5 placeholder-transparent focus:outline-none focus:border-blue-500 resize-none"
+                name="displayName"
+                placeholder="Name"
+              />
+              <label
+                htmlFor="displayName"
+                className="pointer-events-none absolute left-3 top-3 transform -translate-y-1/2 text-xs transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-lg peer-placeholder-shown:text-gray-500 peer-focus:top-3 peer-focus:text-xs peer-focus:text-sky-500"
+              >
+                Bio
               </label>
             </div>
           </div>

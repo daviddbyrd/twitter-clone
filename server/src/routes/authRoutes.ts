111a3c1e07c7d53624ceaf23b00701c927fe1316
daviddbyrd <davbywork@gmail.com>
2025-05-12 17:14:28 +0100

    Refactored index.ts file in backend.

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,34 +1,17 @@
-import express, { Request, Response, NextFunction } from "express";
-import { Pool } from "pg";
-import cors from "cors";
+import express from "express";
+import { asyncHandler } from "../utils/asyncHandler";
 import {
   register,
   login,
   checkUniqueEmail,
   checkUniqueUsername,
 } from "../controllers/authController";
-import { asyncHandler } from "../utils/asyncHandler";
 
-const saltRounds = 10;
+const router = express.Router();
 
-require("dotenv").config();
+router.post("/register", asyncHandler(register));
+router.post("/login", asyncHandler(login));
+router.post("/check-unique-email", asyncHandler(checkUniqueEmail));
+router.post("/check-unique-username", asyncHandler(checkUniqueUsername));
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-const pool = new Pool({
-  user: "davidbyrd",
-  host: "localhost",
-  database: "twitter_clone_db",
-  password: "password",
-  port: 5432,
-});
-
-app.post("/register", asyncHandler(register));
-
-app.post("/login", asyncHandler(login));
-
-app.post("/check-unique-email", asyncHandler(checkUniqueEmail));
-
-app.post("/check-unique-username", asyncHandler(checkUniqueUsername));
+export default router;
--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,6 +1,5 @@
-import express, { Request, Response, NextFunction } from "express";
-import { Pool } from "pg";
-import cors from "cors";
+import express from "express";
+import { asyncHandler } from "../utils/asyncHandler";
 import {
   getPostsById,
   makeReply,
@@ -10,42 +9,17 @@ import {
   unlike,
   postsFromFollowees,
   makePost,
-  getPosts,
 } from "../controllers/postController";
 
-const saltRounds = 10;
+const router = express.Router();
 
-require("dotenv").config();
+router.get("/posts/:id", asyncHandler(getPostsById));
+router.post("/make-reply", asyncHandler(makeReply));
+router.post("/remove-repost", asyncHandler(removeRepost));
+router.post("/repost", asyncHandler(repost));
+router.post("/like", asyncHandler(like));
+router.post("/unlike", asyncHandler(unlike));
+router.get("/posts-from-followees/:user_id", asyncHandler(postsFromFollowees));
+router.post("/make-post", asyncHandler(makePost));
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-const pool = new Pool({
-  user: "davidbyrd",
-  host: "localhost",
-  database: "twitter_clone_db",
-  password: "password",
-  port: 5432,
-});
-
-const asyncHandler =
-  (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-
-app.get("/posts/:id", asyncHandler(getPostsById));
-
-app.post("/make-reply", asyncHandler(makeReply));
-
-app.post("/remove-repost", asyncHandler(removeRepost));
-
-app.post("/repost", asyncHandler(repost));
-
-app.post("/like", asyncHandler(like));
-
-app.post("/unlike", asyncHandler(unlike));
-
-app.get("/posts-from-followees/:user_id", asyncHandler(postsFromFollowees));
-
-app.post("/make-post", asyncHandler(makePost));
+export default router;

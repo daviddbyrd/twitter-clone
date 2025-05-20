0f229c7b82e456d0430116f11d84554923ebfc45
daviddbyrd <davbywork@gmail.com>
2025-05-20 10:48:18 +0100

    Further reformat post.

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -46,17 +46,17 @@ const Post = ({ post, userInfo, onUpdate }: PostProps) => {
       className="w-full bg-white border-gray-100 border-b flex flex-col items-center cursor-pointer hover:bg-gray-50"
       onClick={handleClick}
     >
-      <div className="w-full h-20 flex flex-row items-center">
-        <div className="w-20 h-20 flex items-center justify-center">
+      <div className="w-full flex flex-row items-start justify-start pt-1">
+        <div className="w-15 h-15 flex flex-col items-center justify-start m-3">
           <span onClick={goToProfile} className="cursor-pointer">
             <img
               src={post.profile_picture_url || "/images/default.jpg"}
               alt="Profile picture"
-              className="w-12 h-12 rounded-full mr-auto ml-1"
+              className="w-12 h-12 rounded-full mr-auto"
             />
           </span>
         </div>
-        <div className="flex flex-col justify-start h-full  mt-8">
+        <div className="flex flex-col w-full justify-start h-full mt-2">
           <div>
             <span
               onClick={goToProfile}
@@ -71,88 +71,88 @@ const Post = ({ post, userInfo, onUpdate }: PostProps) => {
               {`@${post.username} • ${relativeTime}`}
             </span>
           </div>
-          <div className="w-full mt-1">{post.content}</div>
-        </div>
-      </div>
-      <div className="flex flex-row h-12 w-full items-center justify-end">
-        <div className="w-40 h-full flex flex-row items-center justify-center">
-          <h3 className="mr-2 text-xl">{post.reply_count}</h3>
-          <span onClick={(e) => e.stopPropagation()}>
-            <button
-              className="rounded-full cursor-pointer"
-              onClick={() => setIsReplying(true)}
-            >
-              <AiOutlineComment className="w-7 h-7 text-gray-500" />
-            </button>
-          </span>
-        </div>
-        <div className="w-40 h-full flex flex-row items-center justify-center">
-          <h3 className="mr-2 text-xl">{post.repost_count}</h3>
-          {post.user_reposted ? (
-            <span onClick={(e) => e.stopPropagation()}>
-              <button
-                className="rounded-full cursor-pointer"
-                onClick={() =>
-                  removeRepost({
-                    postId: post.id,
-                    userId: user?.id,
-                    onSuccess: onUpdate,
-                  })
-                }
-              >
-                <AiOutlineRetweet className="w-7 h-7 text-red-500" />
-              </button>
-            </span>
-          ) : (
-            <span onClick={(e) => e.stopPropagation()}>
-              <button
-                className="cursor-pointer"
-                onClick={() =>
-                  repost({
-                    postId: post.id,
-                    userId: user?.id,
-                    onSuccess: onUpdate,
-                  })
-                }
-              >
-                <AiOutlineRetweet className="w-7 h-7 text-black" />
-              </button>
-            </span>
-          )}
-        </div>
-        <div className="w-40 h-full flex flex-row items-center justify-center">
-          <h3 className="mr-2 text-xl">{post.like_count}</h3>
-          {post.user_liked ? (
-            <span onClick={(e) => e.stopPropagation()}>
-              <button
-                className="rounded-full cursor-pointer"
-                onClick={() =>
-                  unLikePost({
-                    postId: post.id,
-                    userId: user?.id,
-                    onSuccess: onUpdate,
-                  })
-                }
-              >
-                <AiFillHeart className="w-7 h-7 text-red-500" />
-              </button>
-            </span>
-          ) : (
-            <span onClick={(e) => e.stopPropagation()}>
-              <button
-                className="cursor-pointer"
-                onClick={() =>
-                  likePost({
-                    postId: post.id,
-                    userId: user?.id,
-                    onSuccess: onUpdate,
-                  })
-                }
-              >
-                <AiFillHeart className="w-7 h-7 text-black" />
-              </button>
-            </span>
-          )}
+          <div className="mt-1">{post.content}</div>
+          <div className="flex flex-row h-12 items-center justify-between px-10">
+            <div className="w-1/3 h-full flex flex-row items-center justify-center">
+              <h3 className="mr-2 text-xl">{post.reply_count}</h3>
+              <span onClick={(e) => e.stopPropagation()}>
+                <button
+                  className="rounded-full cursor-pointer"
+                  onClick={() => setIsReplying(true)}
+                >
+                  <AiOutlineComment className="w-7 h-7 text-gray-500" />
+                </button>
+              </span>
+            </div>
+            <div className="w-1/3 h-full flex flex-row items-center justify-center">
+              <h3 className="mr-2 text-xl">{post.repost_count}</h3>
+              {post.user_reposted ? (
+                <span onClick={(e) => e.stopPropagation()}>
+                  <button
+                    className="rounded-full cursor-pointer"
+                    onClick={() =>
+                      removeRepost({
+                        postId: post.id,
+                        userId: user?.id,
+                        onSuccess: onUpdate,
+                      })
+                    }
+                  >
+                    <AiOutlineRetweet className="w-7 h-7 text-red-500" />
+                  </button>
+                </span>
+              ) : (
+                <span onClick={(e) => e.stopPropagation()}>
+                  <button
+                    className="cursor-pointer"
+                    onClick={() =>
+                      repost({
+                        postId: post.id,
+                        userId: user?.id,
+                        onSuccess: onUpdate,
+                      })
+                    }
+                  >
+                    <AiOutlineRetweet className="w-7 h-7 text-black" />
+                  </button>
+                </span>
+              )}
+            </div>
+            <div className="w-1/3 h-full flex flex-row items-center justify-center">
+              <h3 className="mr-2 text-xl">{post.like_count}</h3>
+              {post.user_liked ? (
+                <span onClick={(e) => e.stopPropagation()}>
+                  <button
+                    className="rounded-full cursor-pointer"
+                    onClick={() =>
+                      unLikePost({
+                        postId: post.id,
+                        userId: user?.id,
+                        onSuccess: onUpdate,
+                      })
+                    }
+                  >
+                    <AiFillHeart className="w-7 h-7 text-red-500" />
+                  </button>
+                </span>
+              ) : (
+                <span onClick={(e) => e.stopPropagation()}>
+                  <button
+                    className="cursor-pointer"
+                    onClick={() =>
+                      likePost({
+                        postId: post.id,
+                        userId: user?.id,
+                        onSuccess: onUpdate,
+                      })
+                    }
+                  >
+                    <AiFillHeart className="w-7 h-7 text-black" />
+                  </button>
+                </span>
+              )}
+            </div>
+          </div>
         </div>
       </div>
       {isReplying && (
